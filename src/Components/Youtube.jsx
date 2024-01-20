79b8bad022c2976012606b8f2a4fd556ce62747e
vicson <vicsonabuka@gmail.com>
2024-01-20 03:20:09 +0100

    Added Header component

--- src/Components/Youtube.jsx
+++ src/Components/Youtube.jsx
@@ -2,6 +2,27 @@ import './youtube.css'
 function Youtube() {
   return (
     <div>
+      <div className='header'>
+       <div className="left-section">
+        <img className='harmbuger-menu' src='/hamburger-menu.svg' alt='menu tab'/>
+        <img className='youtube-logo' src='youtube-logo.svg' alt='youtube logo'/>
+       </div>
+       <div className="middle-section">
+        <input className='search-bar' type='text' placeholder='Search'/>
+        <button className='search-btn'>
+          <img className='search-icon' src='/search.svg' alt='search btn'/>
+        </button>
+        <button className='voice-btn'>
+          <img className='voice-icon' src='/voice-search-icon.svg' alt='voice btn'/>
+        </button>
+       </div>
+       <div className="right-section">
+        <img className='upload-icon' src='/upload.svg' alt='upload icon'/>
+        <img className='apps-icon' src='/youtube-apps.svg' alt='app icon'/>
+        <img className='notification-icon' src='/notifications.svg'/>
+        <img className='current-user-icon' src='/color2.jpg' alt='user profile'/>
+       </div>
+      </div>
       <div className='container-preview'>
       <div className='video-preview'>
         <div className='thumbnail-row'>
